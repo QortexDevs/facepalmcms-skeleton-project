aa18ae5af15ee8c26ac6e3da22a53d68badacc12
Mikhail Lisnyak <xpundel@gmail.com>
2016-11-15 14:03:19 +0300

    update skeleton resources

---
+++ resources/cms/assets/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by xpundel on 08.04.16.
+ */
+//todo: проверять и стартовать только в нужном модуле
+if (typeof _facepalm != 'undefined') {
+    _facepalm.on('beforeStart', function () {
+ 
+    })
+}
---
+++ resources/site/assets/vendorJs/vendor.js
@@ -0,0 +1,5 @@
+;
+//= ../../../../bower_components/html5shiv/dist/html5shiv.js
+;
+//= ../../../../bower_components/jquery/dist/jquery.js
+;
